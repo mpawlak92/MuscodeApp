936581bc1fe4dfceb10a8178db30449e82708c47
Michal Pawlak <mpawlak.it@gmail.com>
2023-05-17 14:33:05 +0200

    added information about product img to store

--- src/stores/ProductsStore.js
+++ src/stores/ProductsStore.js
@@ -8,21 +8,24 @@ export const useProductsStore = defineStore('productsStore', {
         name: 'iPhone 6s Plus 16GB',
         salePrice: 649,
         price: 1000,
-        currency: '$'
+        currency: '$',
+        img_url: '../assets/img1.png'
       },
       {
         id: 1,
         name: 'iPad Pro 32GB',
         salePrice: 600,
         price: 800,
-        currency: '$'
+        currency: '$',
+        img_url: '../assets/img2.png'
       },
       {
         id: 2,
         name: 'MacBook Pro',
         salePrice: 0,
         price: 8000,
-        currency: 'PLN'
+        currency: 'PLN',
+        img_url: '../assets/img3.png'
       }
     ]
   })
