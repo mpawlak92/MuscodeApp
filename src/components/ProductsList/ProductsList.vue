ee635b22da75e33385dfc95aa33b4194702e1619
Michal Pawlak <mpawlak.it@gmail.com>
2023-05-17 14:24:17 +0200

    created product table

--- src/components/ProductsList/ProductsList.vue
+++ src/components/ProductsList/ProductsList.vue
@@ -1,20 +1,91 @@
 <template>
-  <div class="products-list">
-    <p>product List</p>
-  </div>
+  <CardModel class="card-model-position" cardTitle="Lista Produktów">
+    <template #card-content>
+      <table class="product-table">
+        <thead>
+          <tr>
+            <th class="product-table__id">#</th>
+            <th class="product-table__name">nazwa</th>
+            <th class="product-table__slae-price">promocyjna cena</th>
+            <th class="product-table__price">cena</th>
+            <th class="product-table__currency">waluta</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :key="key" v-for="(product, key) in productsStore.products">
+            <td class="product-table__id">{{ product.id }}</td>
+            <td class="product-table__name">{{ product.name }}</td>
+            <td class="product-table__slae-price">
+              {{ product.salePrice === 0 ? null : product.salePrice }}
+            </td>
+            <td class="product-table__price">{{ product.price }}</td>
+            <td class="product-table__currency">{{ product.currency }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
+  </CardModel>
 </template>
 
 <script setup>
 import '../../assets/base.css'
+import CardModel from '../CardModel/CardModel.vue'
+
+import { useProductsStore } from '../../stores/ProductsStore'
+
+const productsStore = useProductsStore()
 </script>
 
 <style lang="scss" scoped>
-.products-list {
-  width: 100%;
+.card-model-position {
   grid-column: 2/ 4;
   grid-row: 2 / 3;
-  border-radius: 10px;
-  background-color: var(--color-elements-background);
-  box-shadow: 1px 1px 8px 1px var(--color-box-shadow);
+}
+.product-table {
+  width: 100%;
+  margin: 10px 0;
+  border-collapse: collapse;
+  font-size: 0.78rem;
+
+  tr {
+    border-bottom: 1px solid var(--color-border);
+  }
+  tbody tr:hover {
+    background-color: var(--color-table-row);
+    cursor: default;
+  }
+
+  th,
+  td {
+    padding: 5px 5px;
+    padding-left: 10px;
+  }
+
+  thead {
+    text-align: left;
+  }
+  thead tr > * {
+    text-transform: uppercase;
+    font-size: 0.71rem;
+    font-weight: 500;
+    letter-spacing: 0.5px;
+  }
+
+  &__id {
+    width: 6%;
+  }
+
+  &__name {
+    width: 42%;
+  }
+
+  &__sale-price {
+    width: 22%;
+  }
+
+  &__price,
+  &__currency {
+    width: 15%;
+  }
 }
 </style>
