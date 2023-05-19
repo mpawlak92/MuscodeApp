bf11f32d46ff3a98cb43e1728c6f57d392e3e876
Michal Pawlak <mpawlak.it@gmail.com>
2023-05-19 20:31:29 +0200

    created chema of btn

---
+++ src/components/BtnModel/BtnModel.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    :style="{
+      color: color,
+      backgroundColor: bgColor,
+      fontSize: fontSize,
+      border: border,
+      padding: padding,
+      margin: margin
+    }"
+  >
+    {{ props.content }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: {
+    type: String,
+    default: 'Btn name'
+  },
+  color: {
+    type: String,
+    default: '#384a5c'
+  },
+  bgColor: {
+    type: String,
+    default: '#fff'
+  },
+  fontSize: {
+    type: String,
+    default: '1rem'
+  },
+  border: {
+    type: String,
+    default: '1px solid #b2b2b2'
+  },
+  padding: {
+    type: String,
+    default: '5px'
+  },
+  margin: {
+    type: String,
+    default: '5px 5px'
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
